36fbdf10453a02358f370714476757b0417de0a1
edumag <eduardo@mamedu.com>
2015-02-10 14:51:28 +0100

    Improvements in createdoc

--- scripts/doxygen/createdoc.sh
+++ scripts/doxygen/createdoc.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-## @todo Una vez en producción cambiar directorio final de /tmp/ a /web/dox/
-
-possible_languages=(en es ca)
-
 dir_ices="$(dirname "$0")/../../"
 doxygen_conf="scripts/doxygen/doxygen.conf"
 extra_css="scripts/doxygen/extra.css"
@@ -11,6 +7,7 @@ doxygen_header="scripts/doxygen/header.html"
 doxygen_footer="scripts/doxygen/footer.html"
 doxygen_logo="scripts/doxygen/logo.png"
 dir_actual="$(pwd)"
+
 
 cd "$dir_ices"
 
@@ -21,6 +18,15 @@ then
   echo
   exit
 fi
+
+count=0
+for d in docs/* ; do
+  d=`basename $d`
+  if [ "$d" != "img" ] ; then
+    languages[$count]="$d"
+  fi
+  count=$(($count+1))
+done
 
 doxygen="/bin/doxygen"
 log_file=/tmp/doxygen.log
@@ -33,11 +39,11 @@ errors_log=/tmp/doxygen.errors
 function cd_help() {
 
   echo
-  echo 'Usage: createdoc [lang] [version]'
+  echo 'Usage: createdoc [debug]'
   echo
   echo 'This script uses the configuration file doxygen.conf to generate documentation.'
   echo
-  echo Supported languages are: ${possible_languages[*]}
+  echo Detected languages are: ${languages[*]}
   echo
   echo To see debug messages add "debug" as the first parameter.
   echo
@@ -52,22 +58,7 @@ then
   shift 1
 fi
 
-lang=$1
-version=$2
-
-function cd_contains() {
-    local n=$#
-    local value=${!n}
-    for ((i=1;i < $#;i++)) {
-        if [ "${!i}" == "${value}" ]; then
-            return 0
-        fi
-    }
-    return 1
-}
-
-if [ -z "$1" ] || \
-   [ "$1" == "help" ] || \
+if [ "$1" == "help" ] || \
    [ "$1" == "-help" ] || \
    [ "$1" == "-h" ]
    
@@ -87,149 +78,140 @@ if [ $debug == 1 ] ; then
   echo
 fi
 
-cd_contains "${possible_languages[@]}" "$lang"
+for lang in ${languages[*]} ; do
 
-if [ $? == 1 ] 
-then
-  echo
-  echo Language $lang not supported
-  echo
-  cd_help
-  exit 1
-fi
+  if [ "$lang" == "img" ] ; then
+    break
+  fi
 
-# Define language in long format
-#
-# The default language is English, other supported languages are: 
-# Afrikaans, Arabic, Brazilian, Catalan, Chinese, Chinese-Traditional, 
-# Croatian, Czech, Danish, Dutch, Esperanto, Farsi, Finnish, French, German, 
-# Greek, Hungarian, Italian, Japanese, Japanese-en (Japanese with English 
-# messages), Korean, Korean-en, Lithuanian, Norwegian, Macedonian, Persian, 
-# Polish, Portuguese, Romanian, Russian, Serbian, Serbian-Cyrilic, Slovak, 
-# Slovene, Spanish, Swedish, Ukrainian, and Vietnamese.
-# The default language is English, other supported languages are: 
-# Afrikaans, Arabic, Brazilian, Catalan, Chinese, Chinese-Traditional, 
-# Croatian, Czech, Danish, Dutch, Esperanto, Farsi, Finnish, French, German, 
-# Greek, Hungarian, Italian, Japanese, Japanese-en (Japanese with English 
-# messages), Korean, Korean-en, Lithuanian, Norwegian, Macedonian, Persian, 
-# Polish, Portuguese, Romanian, Russian, Serbian, Serbian-Cyrilic, Slovak, 
-# Slovene, Spanish, Swedish, Ukrainian, and Vietnamese.
+  # Define language in long format
+  #
+  # The default language is English, other supported languages are: 
+  # Afrikaans, Arabic, Brazilian, Catalan, Chinese, Chinese-Traditional, 
+  # Croatian, Czech, Danish, Dutch, Esperanto, Farsi, Finnish, French, German, 
+  # Greek, Hungarian, Italian, Japanese, Japanese-en (Japanese with English 
+  # messages), Korean, Korean-en, Lithuanian, Norwegian, Macedonian, Persian, 
+  # Polish, Portuguese, Romanian, Russian, Serbian, Serbian-Cyrilic, Slovak, 
+  # Slovene, Spanish, Swedish, Ukrainian, and Vietnamese.
+  # The default language is English, other supported languages are: 
+  # Afrikaans, Arabic, Brazilian, Catalan, Chinese, Chinese-Traditional, 
+  # Croatian, Czech, Danish, Dutch, Esperanto, Farsi, Finnish, French, German, 
+  # Greek, Hungarian, Italian, Japanese, Japanese-en (Japanese with English 
+  # messages), Korean, Korean-en, Lithuanian, Norwegian, Macedonian, Persian, 
+  # Polish, Portuguese, Romanian, Russian, Serbian, Serbian-Cyrilic, Slovak, 
+  # Slovene, Spanish, Swedish, Ukrainian, and Vietnamese.
 
-case $lang in
-  es)
-    lang_long=Spanish;;
-  en)
-    lang_long=English;;
-  ca)
-    lang_long=Catalan;;
-  fr)
-    lang_long=French;;
-  de)
-    lang_long=German;;
-  *)
-    lang_long=English;;
-esac
+  case $lang in
+    es)
+      lang_long=Spanish;;
+    en)
+      lang_long=English;;
+    ca)
+      lang_long=Catalan;;
+    fr)
+      lang_long=French;;
+    de)
+      lang_long=German;;
+    *)
+      lang_long=English;;
+  esac
 
-# Create documentation directory according to language
+  # Create documentation directory according to language
 
-if [ $debug == 1 ] ; then
-  echo
-  echo Lang: $lang / $lang_long
-  echo Version: $version
-  echo
-fi
+  if [ $debug == 1 ] ; then
+    echo
+    echo Lang: $lang / $lang_long
+    echo
+  fi
 
-[[ -d "docs/tmp" ]] && rm -fr "docs/tmp"
-mkdir "docs/tmp"
-cp -R docs/es/* docs/tmp/
+  [[ -d "docs/tmp" ]] && rm -fr "docs/tmp"
+  mkdir "docs/tmp"
+  cp -R docs/es/* docs/tmp/
 
-if [ "$lang" != "es" ]
-then
-  cp -R docs/$lang/* docs/tmp/
-fi
+  if [ "$lang" != "es" ]
+  then
+    cp -R docs/$lang/* docs/tmp/
+  fi
 
-# Generate final file for doxygen
+  # Generate final file for doxygen
 
-dir_final="$(grep ^OUTPUT_DIRECTORY $doxygen_conf | cut -d= -f 2 | sed 's/^ //')"
-html_output="$(grep ^HTML_OUTPUT $doxygen_conf | cut -d= -f 2 | sed 's/^ //')"
-log_file="$(grep ^WARN_LOGFILE $doxygen_conf | cut -d= -f 2 | sed 's/^ //')"
+  dir_final="$(grep ^OUTPUT_DIRECTORY $doxygen_conf | cut -d= -f 2 | sed 's/^ //')"
+  html_output="$(grep ^HTML_OUTPUT $doxygen_conf | cut -d= -f 2 | sed 's/^ //')"
+  log_file="$(grep ^WARN_LOGFILE $doxygen_conf | cut -d= -f 2 | sed 's/^ //')"
 
-cp "$doxygen_conf" "$tmp_conf"
-echo -e "\n\nOUTPUT_LANGUAGE=$lang_long" >> $tmp_conf
-echo -e "\n\nHTML_OUTPUT=$lang" >> $tmp_conf
+  cp "$doxygen_conf" "$tmp_conf"
+  echo -e "\n\nOUTPUT_LANGUAGE=$lang_long" >> $tmp_conf
+  echo -e "\n\nHTML_OUTPUT=$lang" >> $tmp_conf
 
-if [ -n "$version" ] ; then
-  [[ $debug == 1 ]] && echo -e "\nAdd version: $version"
-  echo -e "\n\nPROJECT_NUMBER=$version" >> $tmp_conf
-fi
+  if [ -f "$extra_css" ] ; then
+    [[ $debug == 1 ]] && echo -e "\nAdd extra css: `pwd`/$extra_css ."
+    echo -e "\n\nHTML_EXTRA_STYLESHEET=`pwd`/$extra_css" >> $tmp_conf
+  fi
 
-if [ -f "$extra_css" ] ; then
-  [[ $debug == 1 ]] && echo -e "\nAdd extra css: `pwd`/$extra_css ."
-  echo -e "\n\nHTML_EXTRA_STYLESHEET=`pwd`/$extra_css" >> $tmp_conf
-fi
+  if [ -f "$doxygen_header" ] ; then
+    [[ $debug == 1 ]] && echo -e "\nAdd header: $doxygen_header"
+    echo -e "\n\nHTML_HEADER=$doxygen_header" >> $tmp_conf
+  fi
 
-if [ -f "$doxygen_header" ] ; then
-  [[ $debug == 1 ]] && echo -e "\nAdd header: $doxygen_header"
-  echo -e "\n\nHTML_HEADER=$doxygen_header" >> $tmp_conf
-fi
+  if [ -f "$doxygen_footer" ] ; then
+    [[ $debug == 1 ]] && echo -e "\nAdd footer: $doxygen_footer"
+    echo -e "\n\nHTML_FOOTER=$doxygen_footer" >> $tmp_conf
+  fi
 
-if [ -f "$doxygen_footer" ] ; then
-  [[ $debug == 1 ]] && echo -e "\nAdd footer: $doxygen_footer"
-  echo -e "\n\nHTML_FOOTER=$doxygen_footer" >> $tmp_conf
-fi
+  if [ -f "$doxygen_logo" ] ; then
+    [[ $debug == 1 ]] && echo -e "\nAdd logo: $doxygen_logo"
+    echo -e "\n\nPROJECT_LOGO=`pwd`/$doxygen_logo" >> $tmp_conf
+    echo -e "\n\nPROJECT_NAME=" >> $tmp_conf
+  fi
 
-if [ -f "$doxygen_logo" ] ; then
-  [[ $debug == 1 ]] && echo -e "\nAdd logo: $doxygen_logo"
-  echo -e "\n\nPROJECT_LOGO=`pwd`/$doxygen_logo" >> $tmp_conf
-  echo -e "\n\nPROJECT_NAME=" >> $tmp_conf
-fi
+  # Delete final dir 
+  if [ -d "${dir_final}${lang}" ]
+  then
+    if [ $debug == 1 ]
+    then
+      echo
+      echo Delete ${dir_final}${lang} first.
+      echo
+    fi
+    rm -fr "${dir_final}${lang}"
+  fi
 
-# Delete final dir 
-if [ -d "${dir_final}${lang}" ]
-then
+  # Run doxygen
   if [ $debug == 1 ]
   then
     echo
-    echo Delete ${dir_final}${lang} first.
+    echo Run doxygen.
     echo
+    cat $tmp_conf | $doxygen - 2> $errors_log
+  else
+    cat $tmp_conf | $doxygen - > /tmp/salida.dox 2> $errors_log
   fi
-  rm -fr "${dir_final}${lang}"
-fi
 
-# Run doxygen
-if [ $debug == 1 ]
-then
-  echo
-  echo Run doxygen.
-  echo
-  cat $tmp_conf | $doxygen - 2> $errors_log
-else
-  cat $tmp_conf | $doxygen - > /tmp/salida.dox 2> $errors_log
-fi
-
-if [ $debug == 1 ] ; then
-  echo
-  echo Final dir: ${dir_final}${lang}
-  echo Output language: $lang_long
-  echo Log file: $log_file
-  echo 
-  echo -e "$conf"
-fi
-
-cd "$dir_actual"
-
-if [ $debug == 1 ] ; then
-  echo
-  read -p "Abrir gvim con errores de doxygen (s/n): " OPCION
-  echo
-  if [ "$OPCION" == 's' ] ; then
-    gvim -c "cfile $log_file" 
-    read
+  if [ $debug == 1 ] ; then
+    echo
+    echo Final dir: ${dir_final}${lang}
+    echo Output language: $lang_long
+    echo Log file: $log_file
+    echo 
+    echo -e "$conf"
   fi
-fi
+
+  cd "$dir_actual"
+
+  if [ $debug == 1 ] ; then
+    echo
+    read -p "Abrir gvim con errores de doxygen (s/n): " OPCION
+    echo
+    if [ "$OPCION" == 's' ] ; then
+      gvim -c "cfile $log_file" 
+      read
+    fi
+  fi
 
 
-## Delete temporal files
-f="docs/tmp" ; [[ -d "$f" ]] && rm -fr "$f"
-f="$tmp_conf" ; [[ -d "$f" ]] && rm -fr "$f"
+  ## Delete temporal files
+  f="docs/tmp" ; [[ -d "$f" ]] && rm -fr "$f"
+  f="$tmp_conf" ; [[ -d "$f" ]] && rm -fr "$f"
+
+done
 
