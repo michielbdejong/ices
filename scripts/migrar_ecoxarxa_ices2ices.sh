09b5ca707064ef2de571c0e35edaeae89056ba1b
edumag <edu@lesolivex.com>
2018-02-18 20:04:57 +0100

    Iniciamos script

---
+++ scripts/migrar_ecoxarxa_ices2ices.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "--"
+echo "-- Migrar ecoxarxa de ICES a ICES."
+echo "--"
+
+ECOXARXA=$1
+DATABASE=${2:-"integralcesservidor"}
+
+echo "--"
+echo "-- ECOXARXA: $ECOXARXA / DATABASE: $DATABASE"
+echo "--"
+
+if [ "$ECOXARXA" == "" ] ; then
+
+  echo
+  echo "Uso: $0 [ID_ECOXARXA] [DATABSE]"
+  echo
+
+fi
+
+mysqldump -w "exchange=$ECOXARXA" \
+--no-create-db \
+--skip-add-drop-table \
+--skip-extended-insert \
+--no-create-info \
+$DATABASE ces_account
