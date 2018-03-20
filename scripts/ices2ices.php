7f539d8fa9b5c816caa330cf3021d03d8b233454
edumag <edu@lesolivex.com>
2018-03-20 07:39:57 +0100

    Add settings in ices2ices

--- scripts/ices2ices.php
+++ scripts/ices2ices.php
@@ -6,19 +6,13 @@
 
 Ficheros finales:
 
-├── announce.csv
-├── balances.csv
-├── groups.csv
-├── levy.csv
-├── offers.csv
-├── recommend.csv
-├── remote.csv
-├── settings.csv
-├── settings-old.csv
-├── trades.csv
-├── users.csv
-├── users-old.csv
-└── wants.csv
+- users
+- announcements
+- exchanges
+- adverts
+- transactions
+- balances
+
  */
 
 $ECOXARXA_ID=31;
@@ -44,15 +38,107 @@ if (!$enlace->set_charset("utf8")) {
 }
 
 /**
- * ==> users.csv <==
- * UID,Password,UserType,Firstname,Surname,OrgName,Address1,Address2,Address3,Postcode,SubArea,DefaultSub,PhoneH,PhoneW,PhoneF,PhoneM,Email,IM,WebSite,DOB,NoEmail1,NoEmail2,NoEmail3,NoEmail4,Hidden,Created,LastAccess,LastEdited,EditedBy,InvNo,OrdNo,Coord,CredLimit,DebLimit,LocalOnly,Notes,Lang,Photo,HideAddr1,HideAddr2,HideAddr3,HideArea,HideCode,HidePhoneH,HidePhoneW,HidePhoneF,HidePhoneM,HideEmail,IdNo,LoginCount,SubsDue,Closed,DateClosed,Translate,Locked,Buddy
+ * settings.csv
+ * @todo CredLim, DebLim.
+ */
+$SQLS['settings']['heads'] = array(
+    'ExchangeID', 'ExchangeTitle', 'ExchangeName', 'ExchangeType',
+    'ExchangeDescr', 'Password', 'Town', 'Logo', 'Administrator', 'Addr1',
+    'Addr2', 'Addr3', 'Postcode', 'Province', 'CountryCode', 'CountryName',
+    'Tel1', 'Tel2', 'Fax', 'TelCode', 'Email', 'InternetMessaging', 'AdminTel',
+    'AdminEmail', 'MemSec', 'MemSecEmail', 'MemSecEmailAlt', 'MemSecPsw',
+    'MemSecTel', 'LevyRate', 'CurName', 'CurNamePlural', 'CurLet',
+    'ConCurName', 'ConCurLet', 'MapAddress', 'WebAddress', 'ReDir', 'Hidden',
+    'Active', 'TimeBased', 'TimeUnit', 'DateAdded', 'DateModified', 'CredLim',
+    'DebLim', 'TimeDiff', 'DaylightSavingOn', 'DaylightSavingOff', 'Language',
+    'DefaultExchanges', 'Cell', 'SubscriptionExchange', 'WelcomeLetter',
+    'InviteLetter', 'InviteLetterHead', 'DoMoney', 'ConRedeemRate', 'HidePsw',
+    'NoDetails', 'BudRate'
+    );
+
+$SQLS['settings']['sql'] = "SELECT
+    id AS 'ExchangeID',
+    shortname AS 'ExchangeTitle',
+    name AS 'ExchangeName',
+    '' AS 'ExchangeType',
+    '' AS 'ExchangeDescr',
+    '' AS 'Password',
+    town AS 'Town',
+    '' AS 'Logo',
+    admin AS 'Administrator',
+    region AS 'Addr1',
+    '' AS 'Addr2',
+    '' AS 'Addr3',
+    '' AS 'Postcode',
+    '' AS 'Province',
+    country AS 'CountryCode',
+    '' AS 'CountryName',
+    '' AS 'Tel1',
+    '' AS 'Tel2',
+    '' AS 'Fax',
+    '' AS 'TelCode',
+    '' AS 'Email',
+    '' AS 'InternetMessaging',
+    '' AS 'AdminTel',
+    '' AS 'AdminEmail',
+    '' AS 'MemSec',
+    '' AS 'MemSecEmail',
+    '' AS 'MemSecEmailAlt',
+    '' AS 'MemSecPsw',
+    '' AS 'MemSecTel',
+    '' AS 'LevyRate',
+    '' AS 'CurName',
+    '' AS 'CurNamePlural',
+    '' AS 'CurLet',
+    '' AS 'ConCurName',
+    '' AS 'ConCurLet',
+    map AS 'MapAddress',
+    website AS 'WebAddress',
+    '' AS 'ReDir',
+    '' AS 'Hidden',
+    state AS 'Active',
+    '' AS 'TimeBased',
+    '' AS 'TimeUnit',
+    created AS 'DateAdded',
+    modified AS 'DateModified',
+    '' AS 'CredLim',
+    '' AS 'DebLim',
+    '' AS 'TimeDiff',
+    '' AS 'DaylightSavingOn',
+    '' AS 'DaylightSavingOff',
+    '' AS 'Language',
+    '' AS 'DefaultExchanges',
+    '' AS 'Cell',
+    '' AS 'SubscriptionExchange',
+    '' AS 'WelcomeLetter',
+    '' AS 'InviteLetter',
+    '' AS 'InviteLetterHead',
+    '' AS 'DoMoney',
+    '' AS 'ConRedeemRate',
+    '' AS 'HidePsw',
+    '' AS 'NoDetails',
+    '' AS 'BudRate'
+    FROM ces_exchange 
+    WHERE id = " . $ECOXARXA_ID . "
+    ";
+
+/**
+ * users.csv
  *
  * 'CredLimit', // @todo Pendiente.
  * 'DebLimit',  // @todo Pendiente.
  */
 
 $SQLS['users']['heads'] = array(
-  'UID','Password','UserType','Firstname','Surname','OrgName','Address1','Address2','Address3','Postcode','SubArea','DefaultSub','PhoneH','PhoneW','PhoneF','PhoneM','Email','IM','WebSite','DOB','NoEmail1','NoEmail2','NoEmail3','NoEmail4','Hidden','Created','LastAccess','LastEdited','EditedBy','InvNo','OrdNo','Coord','CredLimit','DebLimit','LocalOnly','Notes','Lang','Photo','HideAddr1','HideAddr2','HideAddr3','HideArea','HideCode','HidePhoneH','HidePhoneW','HidePhoneF','HidePhoneM','HideEmail','IdNo','LoginCount','SubsDue','Closed','DateClosed','Translate','Locked','Buddy'
+  'UID', 'Password', 'UserType', 'Firstname', 'Surname', 'OrgName', 'Address1',
+  'Address2', 'Address3', 'Postcode', 'SubArea', 'DefaultSub', 'PhoneH',
+  'PhoneW', 'PhoneF', 'PhoneM', 'Email', 'IM', 'WebSite', 'DOB', 'NoEmail1',
+  'NoEmail2', 'NoEmail3', 'NoEmail4', 'Hidden', 'Created', 'LastAccess',
+  'LastEdited', 'EditedBy', 'InvNo', 'OrdNo', 'Coord', 'CredLimit', 'DebLimit',
+  'LocalOnly', 'Notes', 'Lang', 'Photo', 'HideAddr1', 'HideAddr2', 'HideAddr3',
+  'HideArea', 'HideCode', 'HidePhoneH', 'HidePhoneW', 'HidePhoneF',
+  'HidePhoneM', 'HideEmail', 'IdNo', 'LoginCount', 'SubsDue', 'Closed',
+  'DateClosed', 'Translate', 'Locked', 'Buddy'
 );
 $SQLS['users']['sql'] = "SELECT
   ca.name     AS 'UID',
@@ -156,10 +242,7 @@ $SQLS['users']['sql'] = "SELECT
 
 
 /**
- * ==> announce.csv <==
- * ID,Owner,Title,Description,DateAdded, DateEvent,DateExpiry,Keep
- *
- * SELECT * FROM node JOIN field_data_ces_blog_exchange fdcbe ON fdcbe.entity_id = node.nid WHERE fdcbe.ces_blog_exchange_value = 31 
+ * announce.csv
  */
 
 $SQLS['announce']['heads'] = array(
@@ -174,8 +257,8 @@ $SQLS['announce']['sql'] = "SELECT
 		node.changed   AS 'DateEvent',
 		node.changed   AS 'DateEvent',
 		0              AS 'Keep'
-	FROM node 
-	JOIN field_data_ces_blog_exchange fdcbe ON fdcbe.entity_id = node.nid 
+	FROM node
+	JOIN field_data_ces_blog_exchange fdcbe ON fdcbe.entity_id = node.nid
 	JOIN field_data_body fdb ON fdb.entity_id = node.nid
 	WHERE fdcbe.ces_blog_exchange_value = " . $ECOXARXA_ID;
 
@@ -183,26 +266,8 @@ $SQLS['announce']['sql'] = "SELECT
 // ==> balances.csv <==
 // UID,Sales,Income,Purchases,Expenditure,Levy,Balance
 //
-// ==> groups.csv <==
-// GID,GroupTitle,GroupDescr,GroupCat,GroupStarted,GroupModified,NewsUpdated,GroupAccess,GroupOwner,GroupImage,GroupNews,GroupAccount,MemCount,Address1,Address2,Address3,Postcode,Phone,Email,IM,Website,AllMail,AllPhotos,EnableDiscuss,EnableNotice,Active
-//
-// ==> levy.csv <==
-// Revenue,LevyCount,Transferred,TransferCount,Balance
-//
 // ==> offers.csv <==
 // ID,UID,Remote,Category,Subcat,Title,Description,Image,Keys,Rate,ConRate,DateAdded,DateExpires,Hidden
-//
-// ==> recommend.csv <==
-// ID,Recommender,Recommendee,Title,Recommendation,DateAdded,DateEdited,DateExpiry
-//
-// ==> remote.csv <==
-// ID,UID,RXID,Category,Subcategory,Title,Description,Rate,ConRate, DateAdded,DateExpires,Hide
-//
-// ==> settings.csv <==
-// ExchangeID,ExchangeTitle,ExchangeName,ExchangeType,ExchangeDescr,Password,Town,Logo,Administrator,Addr1,Addr2,Addr3,Postcode,Province,CountryCode,CountryName,Tel1,Tel2,Fax,TelCode,Email,InternetMessaging,AdminTel,AdminEmail,MemSec,MemSecEmail,MemSecEmailAlt,MemSecPsw,MemSecTel,LevyRate,CurName,CurNamePlural,CurLet,ConCurName,ConCurLet,MapAddress,WebAddress,ReDir,Hidden,Active,TimeBased,TimeUnit,DateAdded,DateModified,CredLim,DebLim,TimeDiff,DaylightSavingOn,DaylightSavingOff,Language,DefaultExchanges,Cell,SubscriptionExchange,WelcomeLetter,InviteLetter,InviteLetterHead,DoMoney,ConRedeemRate,HidePsw,NoDetails,BudRate,,
-//
-// ==> settings-old.csv <==
-// ExchangeID,ExchangeTitle,ExchangeName,ExchangeType,ExchangeDescr,Password,Town,Logo,Administrator,Addr1,Addr2,Addr3,Postcode,Province,CountryCode,CountryName,Tel1,Tel2,Fax,TelCode,Email,InternetMessaging,AdminTel,AdminEmail,MemSec,MemSecEmail,MemSecEmailAlt,MemSecPsw,MemSecTel,LevyRate,CurName,CurNamePlural,CurLet,ConCurName,ConCurLet,MapAddress,WebAddress,ReDir,Hidden,Active,TimeBased,TimeUnit,DateAdded,DateModified,CredLim,DebLim,TimeDiff,DaylightSavingOn,DaylightSavingOff,Language,DefaultExchanges,Cell,SubscriptionExchange,WelcomeLetter,InviteLetter,InviteLetterHead,DoMoney,ConRedeemRate,HidePsw,NoDetails,BudRate
 //
 // ==> trades.csv <==
 // ID,Seller,Buyer,RemoteExchange,RemoteBuyer,RecordID,DateEntered,EnteredBy,Amount,Levy,LevyRate,Description
