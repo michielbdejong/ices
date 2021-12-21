77e3a012837eb68a8d5cbc64eb0411e678c4857e
Esteve Badia <badia.orive@gmail.com>
2021-12-21 20:14:57 +0100

    komunitin api client credentials for notifications

--- ces_komunitin/includes/Currency.php
+++ ces_komunitin/includes/Currency.php
@@ -24,9 +24,9 @@ class Currency {
     $this->name = $exchange['currencyname'];
     $this->namePlural = $exchange['currenciesname'];
     $this->symbol = $exchange['currencysymbol'];
-    $this->decimals = $exchange['currencyscale'];
-    $this->scale = $exchange['currencyscale'];
-    $this->value = round(pow(10, 6) * $exchange['currencyvalue']);
+    $this->decimals = intval($exchange['currencyscale']);
+    $this->scale = intval($exchange['currencyscale']);
+    $this->value = intval(round(pow(10, 6) * $exchange['currencyvalue']));
   }
 }
 
